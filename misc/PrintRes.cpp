94f7e201649b7ad82e2f7492f6835c5850e75b21
Huang Qinhua <qinhua.huang@polytechnique.edu>
2020-04-29 16:15:42 +0200

    add script for diff momenta

---
+++ misc/PrintRes.cpp
@@ -0,0 +1,19 @@
+void PrintRes (const char* fn, const int eta, const float pt){
+  TFile *f = new TFile(fn);
+  TTree* tracks = (TTree*)f->Get("tracks");
+  tracks->Draw("100*(sqrt(px*px+py*py)/sqrt(gpx*gpx+gpy*gpy)-1)>>h1", "nhits>-9999","goff");
+  TH1F* h1 = (TH1F*)gDirectory->Get("h1");
+  float ptres = h1->GetRMS();
+  float ptres_e = h1->GetRMSError();
+  tracks->Draw("100*(sqrt(px*px+py*py+pz*pz)/sqrt(gpx*gpx+gpy*gpy+gpz*gpz)-1)>>h1", "nhits>-9999","goff");
+  float pres = h1->GetRMS();
+  float pres_e = h1->GetRMSError();
+  ofstream myfile;
+  TString foutname =Form("log_eta%d_pt%02d.log", eta, (int)pt);
+  cout << "Results output to " << foutname << endl;
+  myfile.open (foutname.Data(), ios::out | ios::trunc);
+  myfile << eta << " " << pt << " ";
+  myfile << ptres << " " << ptres_e << " ";
+  myfile << pres << " " << pres_e << endl;
+  myfile.close();
+}
---
+++ misc/script_run_PrintRes.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+WORK="/tmp"
+DETECTOR=${1}
+PT=${2}
+ETA=${3}
+
+if [ -z "${ETA}" ]
+then
+    ETA=0
+fi
+
+source /local/home/qh261761/CEA/EIC/Singularity/setup.sh
+source /cvmfs/sphenix.sdcc.bnl.gov/x8664_sl7/opt/sphenix/core/bin/setup_local.sh $HOME/CEA/EIC/EICsim/g4BMT_strip/source/build/myinstall
+
+pushd "${WORK}"
+if [[ ! -d "MM_EIC" ]]
+then
+    mkdir MM_EIC
+fi
+pushd MM_EIC
+cp -r /local/home/qh261761/CEA/EIC/EICsim/MM_EIC/*.C .
+sed -i "s/\s\{6\}gen->set_eta_range(-\?[0-9]\+,\s[0-9]\+)\;/      gen->set_eta_range(-${ETA}, ${ETA})\;/g" Fun4All_G4_EICDetector.C
+sed -i "s/gen->set_pt_range([0-9]\+\.,\s[0-9]\+\.)\;/gen->set_pt_range(${PT}., ${PT}.)\;/g" Fun4All_G4_EICDetector.C
+root.exe -l -b -q "Fun4All_G4_EICDetector.C(1000)"
+
+FNAME=G4EICDetector.root_g4tracking_eval_eta${ETA}_`printf "%02d" ${PT}`.root
+
+mv G4EICDetector.root_g4tracking_eval.root ${WORK}/${FNAME}
+
+popd
+
+root.exe -l -b -q "PrintRes.cpp(\"${FNAME}\", ${ETA}, ${PT})"
+
+popd
